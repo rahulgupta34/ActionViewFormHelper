d9224aa2dee1b211e53b97cb05599541c56dad2e
rahulgupta34 <rahul.g@simformsolutions.com>
2022-03-15 12:20:25 +0530

    Rails JS Assignement [15/03/2022] create crud for user model and add routes create profile page also and add routes for it create view temlate for profile action profile.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_14_094129) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_15_030220) do
   create_table "addresses", force: :cascade do |t|
     t.string "house_name"
     t.string "street_name"
@@ -90,16 +90,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_14_094129) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "role"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
+    t.string "subscription_email"
+    t.boolean "subcription"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "employees"
