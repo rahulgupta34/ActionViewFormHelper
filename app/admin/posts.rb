1db991525590f956202664d8cc0c377bd041dd78
rahulgupta34 <rahul.g@simformsolutions.com>
2022-03-28 17:41:57 +0530

    add images validaions in post.rb file

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Post do
   menu parent: "Blog"
-  
+ 
   permit_params :title, :body, :picture
   
   # permit_params do
@@ -9,39 +9,55 @@ ActiveAdmin.register Post do
   #   permitted
   # end
 
+  
+
   index do
     selectable_column
     id_column
     column :title
     column :body
     column :picture do |profile|
-      image_tag(profile.picture.url, size: "100x100")
+      if !profile.picture.file.nil?
+        image_tag(profile.picture.url, size: "100x100")
+      end
     end
     actions
   end
 
+ 
+
+  # show do 
+  #   attributes_table do
+  #     row :title
+  #     row :body
+  #     row :picture do |image_profile|
+  #       image_tag(image_profile.picture.url, size: "100x100")
+  #     end
+  #   end
+  # end
+
   show do 
-    attributes_table do
+    render "show", { post_show: post }
+    default_main_content
+  end
+
+  sidebar "Post Details", only: :show do
+    attributes_table_for post do
       row :title
       row :body
-      row :picture do |image_profile|
-        image_tag(image_profile.picture.url, size: "100x100")
-      end
     end
   end
 
-  filter :title
+  filter :title, filters: [:starts_with]
   filter :body
-  filter :created_at
-
-  form do |f|
+ 
+  form multipart: true do |f|
     f.inputs do
       f.input :title
       f.input :body
       f.file_field :picture
-
+      
     end
     f.actions
   end
-  
 end
